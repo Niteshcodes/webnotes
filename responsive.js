0d4934b2e6eb5c30a17617b97ef9a31da818b570
NITESH UPADHAYAY <109732362+Niteshcodes@users.noreply.github.com>
2022-11-25 17:18:41 +0530

    Add files via upload

---
+++ responsive.js
@@ -0,0 +1,35 @@
+
+      let home = document.getElementById("main");
+      let foot = document.getElementById("contect");
+      let h = document.getElementById("hm");
+      let con = document.getElementById("con");
+      let menu= document.getElementById("menu-bar");
+      let onoff=document.getElementById("close-menu")
+      let name=document.getElementById("nav");
+      let ul=document.getElementsByClassName("ul");
+      console.log(name);
+      h.addEventListener("click", () => {
+        foot.style="display:none; opacity:0 ";
+        home.style="display:block; animation:transition 0.7s ";
+      });
+      con.addEventListener("click", () => {
+        home.style="display:none; opacity:0; ";
+        foot.style="display:block; animation:transition 0.7s"
+      });
+
+      menu.addEventListener('click',()=>{
+        name.style="display:flex";
+
+      })
+      onoff.addEventListener('click',()=>{
+        name.style="display:none";
+
+      })
+      ul.addEventListener('click',()=>{
+        name.style="display:none";
+
+      })
+
+
+
+    
